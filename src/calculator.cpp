0bb03c3a206e11b8674584dcb58aa0b5a48caa12
Sagar TV <SVMT1181@MGTECHSOFT.IN>
2026-04-01 11:06:22 +0530

    duplications testing

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -9,6 +9,11 @@ double Calculator::subtract(double a, double b) {
     return a - b;
 }
 
+double Calculator::subtract(double a, double b) {
+    return a - b;
+}
+
+
 double Calculator::multiply(double a, double b) {
     return a * b;
 }
